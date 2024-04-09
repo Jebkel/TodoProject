fc55c0c4e3ccd456fd35a8250d9f2d295d78f1ff
stasc <csv666devil@gmail.com>
2024-04-10 00:32:14 +0300

    Сделал автоматический импорт языков, по названию папки

--- locales/loader.go
+++ locales/loader.go
@@ -1,23 +1,29 @@
 package locales
 
 import (
+	"fmt"
 	"github.com/eduardolat/goeasyi18n"
+	"github.com/labstack/gommon/log"
+	"os"
 )
 
 func Init() *goeasyi18n.I18n {
 	i18n := goeasyi18n.NewI18n()
 
-	ruTranslations, err := goeasyi18n.LoadFromJsonFiles("locales/ru/*.json")
+	entries, err := os.ReadDir("locales/")
 	if err != nil {
-		panic(err)
+		panic("no found directories on locales dir")
 	}
 
-	enTranslations, err := goeasyi18n.LoadFromJsonFiles("locales/en/*.json")
-	if err != nil {
-		panic(err)
+	for _, entry := range entries {
+		if entry.IsDir() {
+			langData, err := goeasyi18n.LoadFromJsonFiles(fmt.Sprintf("locales/%s/*.json", entry.Name()))
+			if err != nil {
+				log.Error(err)
+			}
+			i18n.AddLanguage(entry.Name(), langData)
+		}
 	}
 
-	i18n.AddLanguage("ru", ruTranslations)
-	i18n.AddLanguage("en", enTranslations)
 	return i18n
 }
